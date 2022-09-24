1b9c94e037d950193e27e156f439089e302ac34f
rojasdev <r.rojas@usls.edu.ph>
2022-09-24 09:11:50 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/splashscreendemo/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.demo.splashscreendemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+    }
+}
